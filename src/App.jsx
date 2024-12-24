0c5c4fc4ccd8d2a1753a3468da872b659e54333f
Anisul Islam <anisulislam.pranto@gmail.com>
2024-12-24 17:54:16 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
